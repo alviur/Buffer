15a11cb79ca3f93c968ddf5c90710d17539172e7
Alexander Gomez <alviurlex@gmail.com>
2014-04-12 15:01:56 -0500

    v 0.0-2

--- buffer.cpp
+++ buffer.cpp
@@ -6,9 +6,59 @@ Buffer::Buffer(QWidget *parent) :
     ui(new Ui::Buffer)
 {
     ui->setupUi(this);
+    //Iniciar tiempo de actualizacion
+      FPS=30;
+      QTimer *timer = new QTimer(this);
+      connect(timer, SIGNAL(timeout()), this, SLOT( actualizarFrame()));
+      timer->start(FPS);
+
+      video.open("/home/lex/Cv/Videos/monterrey6.avc");//carga video a usar
+      intTamanoVector=10;
+      contPosicionBuffer=0;
+
+
 }
 
 Buffer::~Buffer()
 {
     delete ui;
+}
+
+/********************************************************************
+Fecha de terminación:
+Autor:Alexander Gomez Villa
+Nombre de metodo: actualizarFrame
+Descripción: se llama cada 33 milisegundos para actualizar la imagen que
+se procesa
+Parámetros de entrada(con explicación cada un de para que se usara):
+Parámetros de salida(con explicación cada un de para que se usara):
+**********************************************************************/
+
+
+Buffer::actualizarFrame()
+{
+
+   if(imgFrameActual)
+   {
+        imgFrameActual.copyTo(imgFrameAnterior);//copia actual en el anterior
+   }
+
+   contPosicionBuffer++;
+   video>>imgFrameActual; //actualizacion imagen
+
+
+   imgFrameActual.copyTo(vectBufferPrincipal[contPosicionBuffer]);
+
+
+
+}
+
+
+
+
+//Cambio del tamaño del buffer
+void Buffer::on_spinBox_valueChanged(int arg1)
+{
+    intTamanoVector=arg1;
+
 }
--- buffer.h
+++ buffer.h
@@ -1,7 +1,20 @@
+/********************************************************************
+Fecha de terminación:
+Autor: Alexander Gomez - German Diez - Sebastian Guzman
+Nombre de clase:Buffer
+Descripción:
+**********************************************************************/
+
 #ifndef BUFFER_H
 #define BUFFER_H
 
 #include <QMainWindow>
+#include<opencv2/opencv.hpp>
+#include<stdio.h>
+#include <QTimer>
+
+using namespace std;
+using namespace cv;
 
 namespace Ui {
 class Buffer;
@@ -14,7 +27,34 @@ class Buffer : public QMainWindow
 public:
     explicit Buffer(QWidget *parent = 0);
     ~Buffer();
+
+    int  intTamanoVector;//sera el tamaño del buffer
+    int contPosicionBuffer;//lleva la cuenta de la posicion actual de buffer
+    std::vector<cv::Mat> vectBufferPrincipal; //vector que cotiene el buffer principal
+    cv::capture video;//video procesado
+    cv::Mat imgFrameActual;
+    cv::Mat imgFrameAnterior;
+
+
+
+    //actualizacion Video
+
+     QTimer* tmrTimer;
+     int     FPS;//frames por segundo
+
+
+public slots:
+
+
+     //Metodo de actualizacion
+
+      void actualizarFrame();
+
+
     
+private slots:
+      void on_spinBox_valueChanged(int arg1);
+
 private:
     Ui::Buffer *ui;
 };
